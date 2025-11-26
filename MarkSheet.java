75080f372f7c511f5e8c716474748e478ae86cb5
elExceed254 <kemboifidel30@gmail.com>
2025-11-26 13:49:38 +0300

    Added java file on student records

---
+++ MarkSheet.java
@@ -0,0 +1,152 @@
+import java.util.Scanner;
+
+public class MarkSheet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System .out.println("First Student Details");
+        String regNo1, fullName1;
+        System.out.print("Reg No: ");
+        regNo1 = sc.next();
+        System.out.print("Name: ");
+        fullName1 = sc.next();
+        int ccs2207_1, ccs2208_1, ccs2209_1, ccs2210_1, ccs2211_1, ccs2215_1, igs2202_1;
+        int total1, average1;
+        System.out.print("CCS 2207: ");
+        ccs2207_1 = sc.nextInt();
+        System.out.print("CCS 2208: ");
+        ccs2208_1 = sc.nextInt();
+        System.out.print("CCS 2209: ");
+        ccs2209_1 = sc.nextInt();
+        System.out.print("CCS 2210: ");
+        ccs2210_1 = sc.nextInt();
+        System.out.print("CCS 2211: ");
+        ccs2211_1 = sc.nextInt();
+        System.out.print("CCS 2215: ");
+        ccs2215_1 = sc.nextInt();
+        System.out.print("IGS 2202: ");
+        igs2202_1 = sc.nextInt();
+        total1 = ccs2207_1 + ccs2208_1 + ccs2209_1 + ccs2210_1 + ccs2211_1 + ccs2215_1 + igs2202_1;
+        average1 = total1 / 7;
+
+        System .out.println("\nTwo Student Details");
+        String regNo2, fullName2;
+        System.out.print("Reg No.: ");
+        regNo2 = sc.next();
+        System.out.print("Name: ");
+        fullName2 = sc.next();
+        int ccs2207_2, ccs2208_2, ccs2209_2, ccs2210_2, ccs2211_2, ccs2215_2, igs2202_2;
+        int total2, average2;
+        System.out.print("CCS 2207: ");
+        ccs2207_2 = sc.nextInt();
+        System.out.print("CCS 2208: ");
+        ccs2208_2 = sc.nextInt();
+        System.out.print("CCS 2209: ");
+        ccs2209_2 = sc.nextInt();
+        System.out.print("CCS 2210: ");
+        ccs2210_2 = sc.nextInt();
+        System.out.print("CCS 2211: ");
+        ccs2211_2 = sc.nextInt();
+        System.out.print("CCS 2215: ");
+        ccs2215_2 = sc.nextInt();
+        System.out.print("IGS 2202: ");
+        igs2202_2 = sc.nextInt();
+        total2 = ccs2207_2 + ccs2208_2 + ccs2209_2 + ccs2210_2 + ccs2211_2 + ccs2215_2 + igs2202_2;
+        average2 = total2 / 7;
+
+        System .out.println("\nThird Student Details");
+        String regNo3, fullName3;
+        System.out.print("Reg No.: ");
+        regNo3 = sc.next();
+        System.out.print("Name: ");
+        fullName3 = sc.next();
+        int ccs2207_3, ccs2208_3, ccs2209_3, ccs2210_3, ccs2211_3, ccs2215_3, igs2202_3;
+        int total3, average3;
+        System.out.print("CCS 2207: ");
+        ccs2207_3 = sc.nextInt();
+        System.out.print("CCS 2208: ");
+        ccs2208_3 = sc.nextInt();
+        System.out.print("CCS 2209: ");
+        ccs2209_3 = sc.nextInt();
+        System.out.print("CCS 2210: ");
+        ccs2210_3 = sc.nextInt();
+        System.out.print("CCS 2211: ");
+        ccs2211_3 = sc.nextInt();
+        System.out.print("CCS 2215: ");
+        ccs2215_3 = sc.nextInt();
+        System.out.print("IGS 2202: ");
+        igs2202_3 = sc.nextInt();
+        total3 = ccs2207_3 + ccs2208_3 + ccs2209_3 + ccs2210_3 + ccs2211_3 + ccs2215_3  + igs2202_3;
+        average3 = total3 / 7;
+
+        System .out.println("\nFourth Student Details");
+        String regNo4, fullName4;
+        System.out.print("Reg No.: ");
+        regNo4 = sc.next();
+        System.out.print("Name: ");
+        fullName4 = sc.next();
+        int ccs2207_4, ccs2208_4, ccs2209_4, ccs2210_4, ccs2211_4, ccs2215_4, igs2202_4;
+        int total4, average4;
+        System.out.print("CCS 2207: ");
+        ccs2207_4 = sc.nextInt();
+        System.out.print("CCS 2208: ");
+        ccs2208_4 = sc.nextInt();
+        System.out.print("CCS 2209: ");
+        ccs2209_4 = sc.nextInt();
+        System.out.print("CCS 2210: ");
+        ccs2210_4 = sc.nextInt();
+        System.out.print("CCS 2211: ");
+        ccs2211_4 = sc.nextInt();
+        System.out.print("CCS 2215: ");
+        ccs2215_4 = sc.nextInt();
+        System.out.print("IGS 2202: ");
+        igs2202_4 = sc.nextInt();
+        total4 = ccs2207_4 + ccs2208_4 + ccs2209_4 + ccs2210_4 + ccs2211_4 + ccs2215_4 + igs2202_4;
+        average4 = total4 / 7;
+
+        System .out.println("\nFifth Student Details");
+        String regNo5, fullName5;
+        System.out.print("Reg No: ");
+        regNo5 = sc.next();
+        System.out.print("Name: ");
+        fullName5 = sc.next();
+        int ccs2207_5, ccs2208_5, ccs2209_5, ccs2210_5, ccs2211_5, ccs2215_5, igs2202_5;
+        int total5, average5;
+        System.out.print("CCS 2207: ");
+        ccs2207_5 = sc.nextInt();
+        System.out.print("CCS 2208: ");
+        ccs2208_5 = sc.nextInt();
+        System.out.print("CCS 2209: ");
+        ccs2209_5 = sc.nextInt();
+        System.out.print("CCS 2210: ");
+        ccs2210_5 = sc.nextInt();
+        System.out.print("CCS 2211: ");
+        ccs2211_5 = sc.nextInt();
+        System.out.print("CCS 2215: ");
+        ccs2215_5 = sc.nextInt();
+        System.out.print("IGS 2202: ");
+        igs2202_5 = sc.nextInt();
+        total5 = ccs2207_5 + ccs2208_5 + ccs2209_5 + ccs2210_5 + ccs2211_5 + ccs2215_5 + igs2202_5;
+        average5 = total5 / 7;
+
+        //Print the scoresheet in tabular format
+        System.out.println("\n\n\t\t\tDedan Kimathi University of Technology");
+        System.out.println("\t\t\tSchool of Computer Science and IT");
+        System.out.println("\t\t\t\tDepartment of Computer Science");
+        System.out.println("\t\t\t\tAcademic Year:2024/2025");
+        System.out.println("\t\t\tSTUDENT MARKSHEET(SECOND YEAR RESULTS)");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Reg No.\t\t\tFull Name\t\t\tCCS 2207s\tCSS 2208\tCCS 2209\tCCS 2210\tCCS 2211\tCCS 2215\tIGS 2202\t\tTOTALS\t\tAVERAGE\t\tSTATUS\t\tGRADE");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(regNo1 + "\t\t" + fullName1 + "\t\t\t" + ccs2207_1 + "\t\t\t" + ccs2208_1 + "\t\t\t" +
+                ccs2209_1 + "\t\t\t" + ccs2210_1 + "\t\t\t" + ccs2211_1 + "\t\t\t" + ccs2215_1 + "\t\t\t" + igs2202_1 + "\t\t\t" + total1 + "\t\t\t" + average1);
+        System.out.println(regNo2 + "\t\t" + fullName2 + "\t\t\t" + ccs2207_2 + "\t\t\t" + ccs2208_2 + "\t\t\t" +
+                ccs2209_2 + "\t\t\t" + ccs2210_2 + "\t\t\t" + ccs2211_2 + "\t\t\t" + ccs2215_2 + "\t\t\t" + igs2202_2 + "\t\t\t" + total2 + "\t\t\t" + average2);
+        System.out.println(regNo3 + "\t\t" + fullName3 + "\t\t\t" + ccs2207_3 + "\t\t\t" + ccs2208_3 + "\t\t\t" +
+                ccs2209_3 + "\t\t\t" + ccs2210_3 + "\t\t\t" + ccs2211_3 + "\t\t\t" + ccs2215_3 + "\t\t\t" + igs2202_3 + "\t\t\t" + total3 + "\t\t\t" + average3);
+        System.out.println(regNo4 + "\t\t" + fullName4 + "\t\t\t" + ccs2207_4 + "\t\t\t" + ccs2208_4 + "\t\t\t" +
+                ccs2209_4 + "\t\t\t" + ccs2210_4 + "\t\t\t" + ccs2211_4 + "\t\t\t" + ccs2215_4 + "\t\t\t" + igs2202_4 + "\t\t\t" + total4 + "\t\t\t" + average4);
+        System.out.println(regNo5 + "\t\t" + fullName5 + "\t\t\t" + ccs2207_5 + "\t\t\t" + ccs2208_5 + "\t\t\t" +
+                ccs2209_5 + "\t\t\t" + ccs2210_5 + "\t\t\t" + ccs2211_5 + "\t\t\t" + ccs2215_5 + "\t\t\t" + igs2202_5 + "\t\t\t" + total5 + "\t\t\t" + average5);
+    }
+}
